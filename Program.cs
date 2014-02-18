0e16651060c3cf0579be2d72464142221965a10e
Toby999 <tobias.fjalling@admeta.com>
2014-02-18 15:19:39 +0100

    initial test commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PoS_InventoryFilterSlim.SimpleTest.MaxThroughput;
+
+namespace PoS_InventoryFilterSlim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 80;
+            new MaxThroughput().RunTest();
+        }
+    }
+}
